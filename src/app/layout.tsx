269f3e65ada08e30df557bf0aacc38d85f4c8963
vzxh <wkq7740@gmail.com>
2024-09-14 14:48:37 +0800

    Revert "init commit"
    This reverts commit f4ccc968bef16753253efd401b90bc7718ad80e2.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
+import { AppFooter } from "@/components/AppFooter";
 import { AppHeader } from "@/components/AppHeader";
 import { regularFont } from "@/utils/fontUtils";
 import classNames from "classnames";
 import type { Metadata } from "next";
+import Image from "next/image";
 import "react-modern-drawer/dist/index.css";
 import "./globals.css";
-import AppProvider from "@/components/provider/AppProvider";
-import { SnackbarInit } from "@/components/SnackbarInit";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "MIZU Home",
@@ -19,19 +20,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        async
+        src="https://analytics.mizu.global/script.js"
+        data-website-id="0fc4fc18-f011-4500-9b81-f80591cd977e"
+      />
+
       <body
         className={classNames(
           regularFont.className,
           "leading-none text-base relative"
         )}
       >
-        <AppProvider>
-          <SnackbarInit />
+        <AppHeader />
 
-          <AppHeader />
+        <div className="pt-[60px] md:pt-[80px]">{children}</div>
 
-          <div className="pt-[60px] md:pt-[80px]">{children}</div>
-        </AppProvider>
+        <AppFooter />
+
+        <div className="w-full h-[400px] md:w-[768px] md:h-[700px] absolute left-0 bottom-0">
+          <Image
+            src="/images/roadmap_glow.svg"
+            layout="fill"
+            alt="icon"
+            priority
+          />
+        </div>
+
+        <div className="w-[180px] h-[180px] 2xl:w-[260px] 2xl:h-[260px] absolute left-0 bottom-0">
+          <Image src="/images/home_bg2.svg" layout="fill" alt="icon" priority />
+        </div>
       </body>
     </html>
   );
---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Mail } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export const AppFooter = () => {
+  return (
+    <div className="z-10 relative h-[60px] md:h-[80px] left-0 right-0 top-0 flex items-center justify-center">
+      <div className="pl-12 md:pl-12 2xl:pl-14 pr-8 w-full flex justify-between items-center">
+        <Image
+          src="/images/logo.svg"
+          alt="MIZU Logo"
+          className="cursor-pointer hidden lg:block"
+          width={83}
+          height={35}
+          priority
+        />
+
+        <Image
+          src="/images/logo.svg"
+          alt="MIZU Logo"
+          className="cursor-pointer block lg:hidden"
+          width={83}
+          height={35}
+          priority
+        />
+
+        <div className="flex items-center">
+          <Link href={"https://x.com/0xmizu_ai"} target="_blank">
+            <Image
+              src="/images/link_x.png"
+              alt="x"
+              className="cursor-pointer "
+              width={25}
+              height={25}
+              priority
+            />
+          </Link>
+
+          <Link href={"mailto:contact@mizu.global"} target="_blank">
+            {/* <Image
+              src="/images/link_mirror.png"
+              alt="x"
+              className="ml-5 cursor-pointer"
+              width={25}
+              height={25}
+              priority
+            /> */}
+            <Mail size={25} className="cursor-pointer ml-5 text-black" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
