1a7c33acdd635605bb154152fcddeb15f876de4e
vzxh <wkq7740@gmail.com>
2024-12-24 09:59:16 +0800

    add privacy policy

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -8,20 +8,31 @@ import logo from "public/images/logo.svg";
 export const AppFooter = () => {
   return (
     <div className="z-10 relative h-[98px] left-0 right-0 top-0 flex items-center justify-center">
-      <div className="pl-12 md:pl-12 2xl:pl-14 pr-8 w-full flex justify-between items-center">
-        <Image
-          src={logo}
-          alt="MIZU Logo"
-          className="w-[83px] h-[30px]"
-          priority
-        />
+      <div className="pl-5 md:pl-12 2xl:pl-14 pr-8 w-full flex justify-between items-center">
+        <div className="flex items-center">
+          <Image
+            src={logo}
+            alt="MIZU Logo"
+            className="w-[63px] md:w-[83px] h-auto"
+            priority
+          />
+
+          <Link
+            href={"https://docs.mizu.technology/misc/privacy-policy"}
+            target="_blank"
+          >
+            <div className="ml-4 md:ml-6 text-white text-[12px] lg:text-[16px] xl:text-[18px]">
+              Privacy Policy
+            </div>
+          </Link>
+        </div>
 
         <div className="flex items-center">
           <Link href={"https://x.com/0xmizu_ai"} target="_blank">
             <Image
               src="/images/link_x.svg"
               alt="x"
-              className="cursor-pointer "
+              className="cursor-pointer w-[20px] md:w-[25px]"
               width={25}
               height={25}
               priority
